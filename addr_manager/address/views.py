e4cae50c67dbbf7c0f8fc922f9edd7439a584a3a
clickingmouse <clickingmouse@users.noreply.github.com>
2020-04-23 21:56:12 +0800

    added api fetch

--- addr_manager/address/views.py
+++ addr_manager/address/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 import requests
+import pprint
+pp = pprint.PrettyPrinter()
 # Create your views here.
 
-def index(requests):
-    return JsonResponse({'Hello':'World'})
+def index(request):
+  url = 'https://www.als.ogcio.gov.hk/lookup?q={}&n=1'
+  #query = 'No. 19 Science Park'
+  query = 'majestic house'
+  query = '80 nathan'
+
+  r = requests.get(url.format(query), headers={'Accept':'application/json', 'Accept-Language':'en'})
+  r = requests.get(url.format(query), headers={'Accept':'application/json'}).json()
+
+
+  pp.pprint(r)
+
+
+  return JsonResponse({'Hello':'World'})
