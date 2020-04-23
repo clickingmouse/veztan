98645c5cec80cf260f6e573934d4224d9b9f55ff
clickingmouse <clickingmouse@users.noreply.github.com>
2020-04-23 21:49:20 +0800

    added basic django-CRID url

--- addr_manager/address/views.py
+++ addr_manager/address/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from django.http import JsonResponse
+import requests
 # Create your views here.
+
+def index(requests):
+    return JsonResponse({'Hello':'World'})
